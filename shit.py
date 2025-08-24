741946dba34e7a364e7f61063236908c04008359
secretsecret-afk <qhzzzz1337@gmail.com>
2025-08-25 00:59:15 +0300

    Create shit.py

---
+++ shit.py
@@ -0,0 +1,22 @@
+import os
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+from dotenv import load_dotenv
+
+# Загружаем переменные окружения
+load_dotenv()
+API_TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=["start"])
+async def send_welcome(message: types.Message):
+    lang = message.from_user.language_code  # язык интерфейса Telegram
+    await message.answer(
+        f"Привет, {message.from_user.first_name}!\n"
+        f"У тебя язык интерфейса: {lang}"
+    )
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
