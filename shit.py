c528e41d71a6045ac836c708eee6ba8721308d43
secretsecret-afk <qhzzzz1337@gmail.com>
2025-08-25 02:55:23 +0300

    Update shit.py

--- shit.py
+++ shit.py
@@ -1,22 +1,28 @@
 import os
+import asyncio
 from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
+from aiogram.types import Message
+from aiogram import F
 from dotenv import load_dotenv
 
-# Загружаем переменные окружения из .env.prem
-load_dotenv(dotenv_path=".env.prem")
+# Загружаем переменные окружения
+load_dotenv(dotenv_path=".env.geneprem")
 API_TOKEN = os.getenv("BOT_TOKEN2")
 
 bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher()
 
-@dp.message_handler(commands=["start"])
-async def send_welcome(message: types.Message):
-    lang = message.from_user.language_code  # язык интерфейса Telegram
+@dp.message(F.text == "/start")
+async def send_welcome(message: Message):
+    lang = message.from_user.language_code
     await message.answer(
         f"Привет, {message.from_user.first_name}!\n"
         f"У тебя язык интерфейса: {lang}"
     )
 
+async def main():
+    dp.startup.register(lambda _: print("Бот запущен!"))
+    await dp.start_polling(bot)
+
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+    asyncio.run(main())
