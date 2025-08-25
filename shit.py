a056bd5b8ebb4ef9d9a11fa11081b16a8d7e5159
secretsecret-afk <qhzzzz1337@gmail.com>
2025-08-25 03:03:39 +0300

    Update shit.py

--- shit.py
+++ shit.py
@@ -1,27 +1,26 @@
-import os
-import asyncio
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher
 from aiogram.types import Message
-from aiogram import F
+import asyncio
+import os
 from dotenv import load_dotenv
 
-# Загружаем переменные окружения
-load_dotenv(dotenv_path=".env.geneprem")
+load_dotenv(dotenv_path=".env.prem")
 API_TOKEN = os.getenv("BOT_TOKEN2")
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
-@dp.message(F.text == "/start")
-async def send_welcome(message: Message):
-    lang = message.from_user.language_code
-    await message.answer(
-        f"Привет, {message.from_user.first_name}!\n"
-        f"У тебя язык интерфейса: {lang}"
-    )
+@dp.message()
+async def start_handler(message: Message):
+    if message.text == "/start":
+        lang = message.from_user.language_code
+        await message.answer(f"Привет, {message.from_user.first_name}!\nЯзык интерфейса: {lang}")
+
+async def on_startup(startup):
+    print("Бот запущен!")
 
 async def main():
-    dp.startup.register(lambda _: print("Бот запущен!!"))
+    dp.startup.register(on_startup)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
