3a3bc9653197f7499084b2bd6cc9f685e443c8d1
secretsecret-afk <qhzzzz1337@gmail.com>
2025-08-25 03:17:26 +0300

    Create g.py

---
+++ g.py
@@ -0,0 +1,26 @@
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+import asyncio, os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path=".env.prem")
+API_TOKEN = os.getenv("BOT_TOKEN2")
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
+
+@dp.message()
+async def start_handler(message: Message):
+    if message.text == "/start":
+        lang = message.from_user.language_code
+        await message.answer(f"Привет, {message.from_user.first_name}!\nЯзык интерфейса: {lang}")
+
+async def on_startup():
+    print("Бот запущен!!")
+
+async def main():
+    dp.startup.register(on_startup)
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
